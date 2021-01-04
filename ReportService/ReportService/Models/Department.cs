7eef8f317caf7e38777e95f80ba60e701970f5c9
MacRandom <macrandomstudio@gmail.com>
2021-01-04 22:05:02 +0700

    Добавил модель подразделений и репозиторий.

---
+++ ReportService/ReportService/Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace ReportService.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ReportService/ReportService/Models/DepartmentRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+namespace ReportService.Models
+{
+    public class DepartmentRepository
+    {
+        private string _connectionString = "Host=192.168.99.100;Username=postgres;Password=1;Database=employee";
+
+        public List<Department> GetDepartments()
+        {
+            var departments = new List<Department>();
+
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                departments = db.Query<Department>("SELECT d.id, d.name from deps d where d.active = true").ToList();
+            }
+
+            return departments;
+        }
+    }
+}
