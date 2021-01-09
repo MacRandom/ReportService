6030040e268e615325a50bba3afc6c0cbe710267
MacRandom <macrandomstudio@gmail.com>
2021-01-09 20:46:52 +0700

    Удалено свойство Department сущности Employee.

--- ReportService/ReportService/Models/Employee.cs
+++ ReportService/ReportService/Models/Employee.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ReportService.Models
+﻿namespace ReportService.Models
 {
     public class Employee
     {
         public string Name { get; set; }
-        public string Department { get; set; }
         public string  Inn { get; set; }
         public int Salary { get; set; }
         public string BuhCode { get; set; }
