67e150b060c9a99c7848a40e22240f9aa18ac342
MacRandom <macrandomstudio@gmail.com>
2021-01-10 00:30:18 +0700

    Отправка сообщения об ошибке пользователю

--- ReportService/ReportService/Startup.cs
+++ ReportService/ReportService/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ReportService.Abstract;
@@ -33,6 +36,21 @@ namespace ReportService
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseExceptionHandler(errorApp =>
+                errorApp.Run(async context =>
+                {
+                    context.Response.ContentType = "text/plain; charset=utf-8";
+
+                    var error = context.Features.Get<IExceptionHandlerFeature>();
+
+                    if (error != null)
+                    {
+                        var exc = error.Error;
+
+                        await context.Response.WriteAsync(exc.Message, Encoding.UTF8);
+                    }
+                }));
 
             app.UseMvc();
         }
